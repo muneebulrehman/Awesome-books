6149bdf82f8dde6824eb09bb97ec3b0930b7dd77
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-01-11 21:12:44 +0530

    Refactored the code on the line of classes

--- index.js
+++ index.js
@@ -2,69 +2,95 @@ const inputTitle = document.querySelector('.title');
 const inputAuthor = document.querySelector('.author');
 const addBtn = document.querySelector('.add-btn');
 const sectionBooks = document.querySelector('.books');
+
 /* eslint-disable */
-if (localStorage.getItem('bookCollection')) {
-	bookCollection = JSON.parse(localStorage.getItem('bookCollection'));
-} else bookCollection = [];
+// if (localStorage.getItem('bookCollection')) {
+// 	bookCollection = JSON.parse(localStorage.getItem('bookCollection'));
+// } else bookCollection = [];
 /* eslint-enable */
-/* eslint-disable */
-function returnBook(title, author, i) {
-  const html = `<div class="each-book">
+
+class BookApp {
+	constructor() {
+		this.bookCollection = localStorage.getItem('bookCollection')
+			? JSON.parse(localStorage.getItem('bookCollection'))
+			: [];
+	}
+
+	// database = () =>
+	// 	window.localStorage.getItem('bookCollection') !== null
+	// 		? JSON.parse(window.localStorage.getItem('bookCollection'))
+	// 		: [];
+
+	/* eslint-disable */
+
+	addBook(title, author) {
+		// sectionBooks.insertAdjacentHTML(
+		// 	'afterbegin',
+		// 	this.returnBook(title, author)
+		// );
+		/* eslint-disable */
+		this.bookCollection.push({ title, author });
+		localStorage.setItem('bookCollection', JSON.stringify(this.bookCollection));
+		loadBooks(this.bookCollection);
+		/* eslint-enable */
+	}
+
+	/* eslint-disable */
+	removeBook(i) {
+		this.bookCollection.splice(i, 1);
+		localStorage.setItem('bookCollection', JSON.stringify(this.bookCollection));
+		loadBooks(this.bookCollection);
+	}
+	/* eslint-enable */
+	returnBook(title, author, i) {
+		const html = `<div class="each-book">
 		  <span class="book-title">${title}</span>
 		  <span class="author">${author}</span>
 		  <button class="remove-btn" data-id=${i}>Remove</button>
 	  </div>`;
-  return html;
+		return html;
+	}
 }
-/* eslint-enable */
+
+const books = new BookApp();
+console.log(books.bookCollection);
+
+addBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+	const title = inputTitle.value;
+	const author = inputAuthor.value;
+	if (title && author) {
+		books.addBook(title, author);
+	}
+
+	inputAuthor.value = inputTitle.value = '';
+});
+
+sectionBooks.addEventListener('click', (e) => {
+	if (e.target.classList.contains('remove-btn')) {
+		const num = +e.target.dataset.id;
+		books.removeBook(num);
+	}
+});
 
 function loadBooks(bookCollection) {
-  sectionBooks.innerHTML = '';
-  /* eslint-disable */
+	sectionBooks.innerHTML = '';
+	/* eslint-disable */
 	if (bookCollection.length > 0) {
 		bookCollection.forEach((book, i) => {
 			sectionBooks.insertAdjacentHTML(
 				'afterbegin',
-				returnBook(book.title, book.author, i),
+				books.returnBook(book.title, book.author, i)
 			);
 		});
 	}
 	/* eslint-enable */
 }
 
-function addBook(title, author) {
-  sectionBooks.insertAdjacentHTML('afterbegin', returnBook(title, author));
-  /* eslint-disable */
-	localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
-	loadBooks(bookCollection);
-	/* eslint-enable */
+function clickToRemove(e) {
+	if (e.target.classList.contains('remove-btn')) {
+		const num = +e.target.dataset.id;
+		books.removeBook(num);
+	}
 }
-/* eslint-disable */
-loadBooks(bookCollection);
-/* eslint-enable */
-addBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  const title = inputTitle.value;
-  const author = inputAuthor.value;
-  /* eslint-disable */
-	bookCollection.push({
-		title,
-		author
-	});
-	addBook(title, author);
-	inputAuthor.value = inputTitle.value = '';
-	/* eslint-enable */
-});
-/* eslint-disable */
-function removeBook(i) {
-	bookCollection.splice(i, 1);
-	localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
-	loadBooks(bookCollection);
-};
-/* eslint-enable */
-sectionBooks.addEventListener('click', (e) => {
-  if (e.target.classList.contains('remove-btn')) {
-    const num = +e.target.dataset.id;
-    removeBook(num);
-  }
-});
+loadBooks(books.bookCollection);
